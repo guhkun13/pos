2482d385fb7314dba59a686f665663af142bd1e7
username <user@com.com>
2023-09-01 11:47:18 +0800

    simplify structure

--- config/env.go
+++ config/env.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
 type EnvironmentVariables struct {
 	Database struct {
 		Username string `mapstructure:"USERNAME"`
@@ -8,4 +13,21 @@ type EnvironmentVariables struct {
 		Port     string `mapstructure:"PORT"`
 		Name     string `mapstructure:"NAME"`
 	} `mapstructure:"DATABASE"`
+}
+
+func LoadEnv() (envVar EnvironmentVariables, err error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Error().Err(err).Msg("viper error read config")
+	}
+
+	err = viper.Unmarshal(&envVar)
+	if err != nil {
+		log.Error().Err(err).Msg("viper error unmarshal config")
+	}
+
+	return
 }
